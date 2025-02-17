35bd3ee00fb9dd4ccb627cbb169b9333c016de5a
YatenArora05 <154497355+YatenArora05@users.noreply.github.com>
2025-02-17 22:50:49 +0530

    Cloudinary And Multer

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApriError extends Error{
             statusCode,
             message="Something went wrong",
             errors=[],
-            statck=""
+            stack=""
     ){
         super(message);
         this.statusCode=statusCode
